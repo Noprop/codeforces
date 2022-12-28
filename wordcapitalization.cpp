b7f1feab347d4349493c03e1132f3cd20a85a6ee
Noprop <12greg21@gmail.com>
2022-12-28 16:58:40 -0500

    281A Word Capitalization

---
+++ wordcapitalization.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+  string s;
+  getline(cin, s);
+  s[0] = toupper(s[0]);
+  cout << s;
+	
+  return 0;
+}
