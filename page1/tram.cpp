a2fbd9112e5a25d271cbc6603c1b40325a66c08d
Noprop <12greg21@gmail.com>
2022-12-29 13:51:44 -0500

    116A Tram

--- page1/tram.cpp
+++ page1/tram.cpp
@@ -8,6 +8,21 @@ int main()
 {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
+
+  int n, p=0;
+  int maxP=0;
+  cin >> n;
+
+  while (n--) {
+    int exit, enter;
+    cin >> exit >> enter;
+
+    p -= exit;
+    p += enter;
+    maxP = max(maxP, p);
+  }
+
+  cout << maxP;
 	
   return 0;
 }
