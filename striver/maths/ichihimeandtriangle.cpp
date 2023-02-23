4b6474b35b808a8c51cc15575dae788778cf9d67
Noprop <12greg21@gmail.com>
2023-02-23 18:02:51 -0500

    1337A Ichihime and Triangle

---
+++ striver/maths/ichihimeandtriangle.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int a,b,c,d;
+        cin>>a>>b>>c>>d;
+
+        cout << b << " " << c << " " << c << "\n";
+    }
+	
+  return 0;
+}
