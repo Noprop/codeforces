fc97f86db9ea7d2a152e835be2238dd26364e726
Noprop <12greg21@gmail.com>
2023-02-25 14:22:08 -0500

    1360A Minimal Square

---
+++ striver/maths/minimalsquare.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int a,b;
+        cin>>a>>b;
+
+        cout << pow(max(min(a,b)*2, max(a,b)), 2) << endl;
+    }
+	
+    return 0;
+}
